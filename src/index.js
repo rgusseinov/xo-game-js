844608587e114a58f4381a2c6486bce22b34f52f
Ruslan Gusseinov <r.gusseinov@mail.ru>
2021-11-15 23:40:12 +0600

    feat: add html and basic event

--- src/index.js
+++ src/index.js
@@ -1,42 +1,20 @@
+const PLAYER = 'X';
 
-/* function getPosts(){
-  return new Promise((resolve, reject) => {
-    setTimeout(() => resolve('timer for 3 second'), 3000);
-  })
+const arr = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0]
+];
+
+
+const boxes = document.querySelectorAll('.box');
+boxes.forEach(box => box.addEventListener('click', handleBoxClick));
+
+function handleBoxClick(evt){
+  const current = evt.target;
+  current.innerHTML = PLAYER;
+
+  arr[1][2] = 55;
+  console.log(arr);
 }
 
-// console.log(getPosts());
-
-getPosts().then((response) => {
-  console.log(response);
-  return new Promise((resolve, reject) => {
-    setTimeout(() => resolve('timer for 1 second'), 1000);
-  })
-}).then(data => {
-  console.log(data);
-})
-*/
-
-/* const data = fetch('https://jsonplaceholder.typicode.com/posts');
-
-data.then((result) => {
-  return result.json()
-}).then(response => {
-  console.log(response)
-}) */
-
-const URL = 'https://jsonplaceholder.typicode.com/posts';
-
-function getPosts(){
-  const response = fetch(URL);
-  return response;
-}
-
-getPosts()
-  .then(res => res.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(err => {
-    console.error(err);
-  })
