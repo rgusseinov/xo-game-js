24ef935352fdcd1f7843286c48e84b2715cc0ce6
= <r.@mail.ru>
2021-11-15 21:14:27 +0600

    initgit remote add origin https://github.com/rgusseinov/xo-game-js.git

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
- 
+
+/* function getPosts(){
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve('timer for 3 second'), 3000);
+  })
+}
+
+// console.log(getPosts());
+
+getPosts().then((response) => {
+  console.log(response);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve('timer for 1 second'), 1000);
+  })
+}).then(data => {
+  console.log(data);
+})
+*/
+
+/* const data = fetch('https://jsonplaceholder.typicode.com/posts');
+
+data.then((result) => {
+  return result.json()
+}).then(response => {
+  console.log(response)
+}) */
+
+const URL = 'https://jsonplaceholder.typicode.com/posts';
+
+function getPosts(){
+  const response = fetch(URL);
+  return response;
+}
+
+getPosts()
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.error(err);
+  })
